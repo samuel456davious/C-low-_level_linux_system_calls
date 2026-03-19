64519dada10bcdda258a096f51dcb932b0b73fdc
samuel456davious <daviousgrigi@gmail.com>
2026-03-20 02:33:37 +0300

    added mac_address_changer

--- mac_address_changer.c
+++ mac_address_changer.c
@@ -14,7 +14,13 @@ int main() {
     // 1. Bring interface down
     system("sudo ip link set dev wlan0 down");
 
-    
+    // 2. Change the MAC
+    sprintf(command, "sudo ip link set dev wlan0 address %s", mac);
+    if (system(command) == 0) {
+        printf("MAC address changed successfully!\n");
+    } else {
+        fprintf(stderr, "Failed to change MAC address.\n");
+    }
 
     // 3. Bring interface back up
     system("sudo ip link set dev wlan0 up");
